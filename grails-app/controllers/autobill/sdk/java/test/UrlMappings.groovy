d091111b9d1ae9dbd059a6e1ea12f687d3331c20
arif <arif@bitmascot.com>
2020-11-10 16:00:29 +1100

    Base

---
+++ grails-app/controllers/autobill/sdk/java/test/UrlMappings.groovy
@@ -0,0 +1,16 @@
+package autobill.sdk.java.test
+
+class UrlMappings {
+
+    static mappings = {
+        "/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
+
+        "/"(view:"/index")
+        "500"(view:'/error')
+        "404"(view:'/notFound')
+    }
+}
---
+++ grails-app/init/autobill/sdk/java/test/BootStrap.groovy
@@ -0,0 +1,9 @@
+package autobill.sdk.java.test
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
